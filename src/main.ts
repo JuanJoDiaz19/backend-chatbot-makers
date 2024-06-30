3e1f133798416bf654920d54a24a0b5ed5f2ea80
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-06-30 00:36:09 -0500

    feat: fixing the cors error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ProductsService } from './products/services/products.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:'*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   await app.listen(4000);
 }
 bootstrap();
