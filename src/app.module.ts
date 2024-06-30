0aa3b8123cc6a17b5080eaab5c9fcec5e86bc762
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-06-29 19:12:09 -0500

    feat: adding the project structure

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ProductsModule, 
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    database: process.env.DB_NAME, 
+    username: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    autoLoadEntities: true, 
+    synchronize: true ,
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
